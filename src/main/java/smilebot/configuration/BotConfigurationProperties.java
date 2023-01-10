e8292a69532fa4b5aa33f2279920d6c3ed1ef6ea
TrixiEther <trixiether@gmail.com>
2023-01-10 17:31:57 +0300

    TrixiEther | Enhancement | Docker-Compose
    Set @Value for token

--- src/main/java/smilebot/configuration/BotConfigurationProperties.java
+++ src/main/java/smilebot/configuration/BotConfigurationProperties.java
@@ -1,6 +1,7 @@
 package smilebot.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -14,10 +15,12 @@ public class BotConfigurationProperties {
     @Autowired
     private Environment env;
 
+    @Value("${TOKEN}")
     private String token;
     private List<BotCommand> commands;
 
     public void setToken(String t) {
+        System.out.println("Setting TOKEN...");
         if (env.getProperty("TOKEN") == null) {
             System.out.println("Using TOKEN from application.properties");
             token = t;
